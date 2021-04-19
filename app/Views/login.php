bcd081b5f36a98647d3164f2fa141ac18b6f4592
Liz Fernandez <lyzfernandez6@gmail.com>
2021-04-19 14:38:11 -0400

    login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,40 +1,28 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset = " utf-8 ">
+    <title>Login design| XD</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-    <title>login</title>
-  </head>
-  <body >
+    
+    <link rel="stylesheet" href="<?= base_url() ?>/plantilla/css/login.css">
+  </head >
+  <body>
 
     <div  class = " login-box " >
       <img  src = "Imagenes/usuario.png" class = " avatar " alt = " Imagen de Avatar " >
+      <img  src = "Imagenes/iniciar-sesion.png" class = " avatarIS " alt = " Imagen de AvatarIS " >
       <h1> Iniciar sesión </h1>
-      <div  class="row"> 
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4">
-
-        <form>
+      <form>
         <!--ENTRADA DE USUARIO-->
         <label  para = " nombre de usuario " > USUARIO </label>
-        <input  type = " text " placeholder = " usuario " class="form-control">
+        <input  type = " text " placeholder = " Email " >
         <!--ENTRADA DE CONTRASEÑA-->
         <label for = " contraseña " > CONTRASEÑA </label>
-        <input type = " password " placeholder = " Password " class="form-control">
-        <a  href = " # " > ¿Olvidaste  tu contraseña? </a>
-        <br>
-        <button class="btn btn-primary"> <img  src = "Imagenes/iniciar-sesion.png" class = " avatarIS " alt = " Imagen de AvatarIS " > Iniciar Sesión</button>
-        <!--<input type = " submit " value = " Iniciar sesión ">-->
+        <input type = " password " placeholder = " Password ">
+        <a  href = " # " > ¿Olvidaste  tu contraseña? </a><br>
+        <input type = " submit " value = " Iniciar sesión ">
       </form >
-
-        </div>
-        <div class="col-sm-4"></div>
-      </div>
     </div>
   </body>
 </html>
