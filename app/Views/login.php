30c68aac48c9b233805ba9daec657a64f08dfb80
Liz Fernandez <lyzfernandez6@gmail.com>
2021-04-19 09:50:58 -0400

    login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,6 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
+<!DOCTYPE html>
+<html>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,35 +8,33 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Login</title>
+    <title>login</title>
   </head>
-  <body>
-<div class="container">
-<div class="row">
-<div class="col-sm-4"></div>
-<div class="col-sm-4">
-<h1>Inicio de Sesión</h1>
-<form action="<?php echo base_url('/login')  ?>" method=¨POST>
-<label for="usuario">Usuario</label>
-<input type="text" name="usuario" class="form-control" required=""> 
-<label for="password">Password</label>
-<input type="password" name="password" class="form-control" required="" >
-<br>
-<button class="btn btn-primary">Ingresar</button>
-</form>
-</div>
-<div class="col-sm-4"></div>
-</div>
-</div>
-    <!-- Optional JavaScript; choose one of the two! -->
+  <body >
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <div  class = " login-box " >
+      <img  src = "Imagenes/usuario.png" class = " avatar " alt = " Imagen de Avatar " >
+      <h1> Iniciar sesión </h1>
+      <div  class="row"> 
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
+        <form>
+        <!--ENTRADA DE USUARIO-->
+        <label  para = " nombre de usuario " > USUARIO </label>
+        <input  type = " text " placeholder = " usuario " class="form-control">
+        <!--ENTRADA DE CONTRASEÑA-->
+        <label for = " contraseña " > CONTRASEÑA </label>
+        <input type = " password " placeholder = " Password " class="form-control">
+        <a  href = " # " > ¿Olvidaste  tu contraseña? </a>
+        <br>
+        <button class="btn btn-primary"> <img  src = "Imagenes/iniciar-sesion.png" class = " avatarIS " alt = " Imagen de AvatarIS " > Iniciar Sesión</button>
+        <!--<input type = " submit " value = " Iniciar sesión ">-->
+      </form >
+
+        </div>
+        <div class="col-sm-4"></div>
+      </div>
+    </div>
   </body>
 </html>
